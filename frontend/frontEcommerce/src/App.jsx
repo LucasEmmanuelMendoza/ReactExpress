601be2f66d97b975580448b4ad08f8ede1ca492c
Lucas Mendoza <mendozalucas001@gmail.com>
2024-09-30 20:47:48 -0300

    cambios front

--- frontend/frontEcommerce/src/App.jsx
+++ frontend/frontEcommerce/src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css'
-import { ItemListContainer } from './components/ItemListContainer'
+import { ItemListContainer } from './components/items/ItemListContainer'
 
 function App() {
   return (
--- frontend/frontEcommerce/src/components/items/ItemList.jsx
+++ frontend/frontEcommerce/src/components/items/ItemList.jsx
@@ -1,10 +1,10 @@
 import {Item} from './Item';
-import '../App.css' 
-import '../index.css' 
+import '../../App.css'
+import '../../index.css'
 
 export const ItemList = ({items}) => {
     return(
-        <div className="cardsContainer Container">
+        <div className="cardsContainer">
             {items.map((item) => <Item item={item} key={item._id}/>)}
         </div>
     )
