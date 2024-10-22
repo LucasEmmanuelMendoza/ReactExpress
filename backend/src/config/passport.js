3df551c35c6b923f5845612e74c618f64d2bd3bc
Lucas <mendozalucas001@gmail.com>
2024-10-22 05:02:25 -0300

    passport register

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -2,8 +2,10 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const UserManager = require('../manager/user.manager.js');
 const userManager = new UserManager();
-const isValidPassword = require('../utils/bcrypt.js')
-
+const isValidPassword = require('../utils/bcrypt.js');
+const createHash = require('../utils/bcrypt.js')
+const CartManager = require('../manager/cart.manager.js');
+const cartManager = new CartManager();
 
 const validateEmail = (email) => /\S+@\S+\.\S+/.test(email)
 
@@ -16,14 +18,14 @@ const initializePassport = () => {
                 const userData = req.body
                 let user = await userManager.getUserByUsername(username);
                 if(user !== null){
-                    if(isValidPassword(user, password)){
+                    if(isValidPassword(user, userData.password)){
                         user.last_connection = new Date()
                         return done(null, user)
                     }else{
-                        throw new Error('Invalid Password or User}')
+                        throw new Error('Invalid Password or User')
                     }
                 }else{
-                    throw new Error('Invalid Password or User}')
+                    throw new Error('Invalid Password or User')
                 }
             }catch(err){
                 return done(err);
@@ -31,12 +33,48 @@ const initializePassport = () => {
         }
     ))
 
+    passport.use('register', new LocalStrategy(
+        {usernameField: 'email', passReqToCallback: true},
+        async(req, username, password, done)=>{
+            try{
+                if(await userManager.getUserByUsername(username)){
+                    return done('Usuario existente!')
+                }else{
+                    const userData = req.body
+                    const cart = await cartManager.createCart();
+                    const newUser = {
+                        email: username,
+                        password: createHash(password),
+                        first_name: userData.first_name,
+                        last_name: userData.last_name,
+                        age: userData.age,
+                        cartId: cart._id
+                    }
+                    for(const key of Object.keys(newUser)){
+                        const field = newUser[key];
+                        if(typeof(field) === 'string' && field.trim() === '' || field === null){
+                            return done('Error tratando de registrar un usuario')
+                        }
+                    }
+                    if(!validateEmail(username)){
+                        return done('Error tratando de registrar un usuario')
+                    }
+
+                    let result = await userManager.addUser(newUser);
+                    return done(null, result);
+                }
+            }catch(err){
+                res.status(500).send('Error inesperado');
+            }
+        }
+    ))
+
     passport.serializeUser(function(user, done){
-        done(null, user);
+        done(null, user)
     })
 
     passport.deserializeUser(function(user, done){
-        done(null, user);
+        done(null, user)
     })
 }
 
--- backend/src/dao/db/models/user.model.js
+++ backend/src/dao/db/models/user.model.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
     first_name:{
         type: String,
         required: true
